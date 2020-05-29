63a547de547d853647f21c699b92f781dd7b90b2
ErnestoFlo <65908625+ErnestoFlo@users.noreply.github.com>
2020-05-29 15:45:11 -0600

    Validacion de numeros positivos, negativos, cero, e ingreso de caracteres no permitidos

---
+++ Validacion de Numeros/Validacion de Numeros/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.btnValidar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(54, 50)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumero.TabIndex = 0
+        '
+        'btnValidar
+        '
+        Me.btnValidar.Location = New System.Drawing.Point(67, 106)
+        Me.btnValidar.Name = "btnValidar"
+        Me.btnValidar.Size = New System.Drawing.Size(75, 23)
+        Me.btnValidar.TabIndex = 1
+        Me.btnValidar.Text = "Validar"
+        Me.btnValidar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(213, 193)
+        Me.Controls.Add(Me.btnValidar)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents btnValidar As Button
+End Class
---
+++ Validacion de Numeros/Validacion de Numeros/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnValidar.Click
+        Dim valor As Int16
+        If txtNumero.Text = "" Then
+            MsgBox("Ingrese un numero en la casilla", vbInformation)
+        ElseIf Not IsNumeric(txtNumero.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtNumero.Text = ""
+        ElseIf IsNumeric(valor) Then
+            valor = Val(txtNumero.Text)
+            If valor = 0 Then
+                MsgBox("Numero cero", vbInformation)
+            ElseIf valor > 0 Then
+                MsgBox("Numero Positivo")
+            ElseIf valor < 0 Then
+                MsgBox("Numero Negativo")
+            End If
+
+        End If
+    End Sub
+End Class
